4a87f1b00371e72c8dd5ba283c11c054dd645f2d
TURY <2021@upqro.edu.mx>
2022-11-16 10:15:41 -0500

    login parte 1

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,12 +1,44 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using proyecto24BM.Context;
+using System;
+using System.Linq;
 
 namespace proyecto24BM.Controllers
 {
     public class LoginController : Controller
     {
-        public IActionResult Login()
+        private readonly ApplicationDdContext _context;
+
+        public LoginController(ApplicationDdContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
         {
             return View();
+        }
+
+        public JsonResult LoginUser(string user, string Password)
+        {
+            try
+            {
+                var response = _context.usuarios.Where(x => x.User == user && x.Password == Password);
+                if(response.Count() > 0)
+                {
+                    //se va a logear
+                    return Json(new { Succes = true});
+                }
+                else
+                {
+                    //Errors
+                    return Json(new { Succes = false });
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Surgio un eror" + ex.Message);
+            }
         }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -53,7 +53,7 @@ namespace proyecto24BM
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Login}/{action=Index}/{id?}");
             });
         }
     }
