6b2d0a25b9b999e53d09c962ccb2b9fa23b86360
TURY <2021@upqro.edu.mx>
2022-11-22 13:38:54 -0500

    Crear articulo

--- Controllers/ArticulosController.cs
+++ Controllers/ArticulosController.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using proyecto24BM.Context;
+using proyecto24BM.Models;
+using System;
 using System.Threading.Tasks;
 
 namespace proyecto24BM.Controllers
@@ -13,12 +17,39 @@ namespace proyecto24BM.Controllers
         {
             _context = context;
         }
-        public async Task<IActionResult> Articulos()
+
+        SqlConnection connection = new SqlConnection("Data Source = DESKTOP-GBA3LGV; initial catalog = proyecto24BM; Integrated Security = true;");
+        public async Task<IActionResult> Index()
         {
+            /*try
+            {
+                var response = await connection.QueryAsync<Articulos>();
+            }catch (Exception ex)
+            {
+
+            }*/
+
             var response = await _context.articulos.ToListAsync();
 
             return View(response);
         }
-        
+
+        [HttpPost]
+        public async Task<IActionResult> Crear(Articulos response)
+        {
+            try
+            {
+                if (response != null)
+                {
+                    await connection.QueryAsync<Articulos>("spInsertArticulo", new { response.Nombre, response.Descripcion, response.Urlimg });
+                    return RedirectToAction(nameof(Index));
+                }
+                return View();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Errors papu " + ex.Message);
+            }
+        }
     }
 }
